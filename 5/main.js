a87f38357e9cafe2186618bbeea09914fbe735b3
AyaanJ <66999423+12345678-tech@users.noreply.github.com>
2022-06-18 13:10:13 +0530

    Add files via upload
    Question-5

---
+++ 5/main.js
@@ -0,0 +1,11 @@
+function cakes(recipe, available) {
+  return Object.keys(recipe).reduce(function (val, ingredient) {
+    console.log(val)
+    console.log(ingredient)
+    return Infinity
+  }, Infinity)
+}
+cakes(
+  { flour: 500, sugar: 200, eggs: 1 },
+  { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }
+)
